056d2cd24f2ef0422a28b97ca673fb21ce43dbcf
Aaron Hazzard <62683196+A-Hazzard@users.noreply.github.com>
2022-11-16 22:00:23 -0400

    Created Animations on all pages

--- AutoServe/src/main/webapp/script.js
+++ AutoServe/src/main/webapp/script.js
@@ -27,9 +27,13 @@ $("#footer-links").load("pages/states/footer/carLinks.txt");
                     $("main").hide().load("pages/states/loginPage.txt").fadeIn("slow");
                 })
 
-                    //loads memberPage on click
+                    //loads obutUsPage on click
                     $("#aboutUsPage").on('click', function(){
                         $("main").hide().load("pages/states/aboutUsPage.txt").fadeIn("slow");
+                    });
+                        //loads servicesPage on click
+                    $("#servicesPage").on('click', function(){
+                        $("main").hide().load("pages/states/servicesPage.txt").fadeIn("fast");
                     });
 
     //changes style of links in the footer on click
@@ -121,6 +125,33 @@ $("#footer-links").load("pages/states/footer/carLinks.txt");
         }
     });
 
+
+    AOS.init();
+
+    // You can also pass an optional settings object
+    // below listed default settings
+    AOS.init({
+      // Global settings:
+      disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+      startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+      initClassName: 'aos-init', // class applied after initialization
+      animatedClassName: 'aos-animate', // class applied on animation
+      useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+      disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+      debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+      throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+      
+    
+      // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+      offset: 120, // offset (in px) from the original trigger point
+      delay: 0, // values from 0 to 3000, with step 50ms
+      duration: 400, // values from 0 to 3000, with step 50ms
+      easing: 'ease', // default easing for AOS animations
+      once: false, // whether animation should happen only once - while scrolling down
+      mirror: false, // whether elements should animate out while scrolling past them
+      anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+    
+    });
 });//end main function
 
 
